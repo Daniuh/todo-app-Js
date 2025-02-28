64ce8cd74e71cb642e657946df7e3f966e8b5b7c
Daniel Naranjo <naranjobenavidesd@gmail.com>
2025-02-28 14:24:44 +0100

    Se crea una opción para que la app pase de dark a light

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-import './style.css'
+import './src/css/style.css';
+import './src/css/style-dark-mode.css';
+
 import { App } from './src/todos/app';
 
 App('#app');
---
+++ src/js/dark-mode.js
@@ -0,0 +1,21 @@
+
+        const toggleButton = document.getElementById("theme-toggle");
+        const body = document.body;
+
+        toggleButton.addEventListener("click", () => {
+        body.classList.toggle("dark-mode");
+        
+        if (body.classList.contains("dark-mode")) {
+            toggleButton.textContent = "☀️ Modo Claro";
+            localStorage.setItem("theme", "dark");
+        } else {
+            toggleButton.textContent = "🌙 Modo Oscuro";
+            localStorage.setItem("theme", "light");
+        }
+        });
+
+        // Mantener el modo elegido al recargar
+        if (localStorage.getItem("theme") === "dark") {
+        body.classList.add("dark-mode");
+        toggleButton.textContent = "☀️ Modo Claro";
+        }
