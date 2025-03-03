10a83dcb63bf716167fd6b1d28ad32181eb3ff90
Daniel Naranjo <naranjobenavidesd@gmail.com>
2025-03-03 07:50:45 +0100

    Se crea el store base de nuestra app

--- main.js
+++ main.js
@@ -2,5 +2,8 @@ import './src/css/style.css';
 import './src/css/style-dark-mode.css';
 
 import { App } from './src/todos/app';
+import todoStore from "./src/store/todo-store";
+
+todoStore.initStore();
 
 App('#app');
