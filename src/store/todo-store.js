e7d8a6acc54ae50f23db911e90850f1966ab4983
Daniel Naranjo <naranjobenavidesd@gmail.com>
2025-03-04 10:30:38 +0100

    Se crean las funciones que se van a utilizar en la app

--- src/store/todo-store.js
+++ src/store/todo-store.js
@@ -21,6 +21,56 @@ const initStore = () => {
     
 }
 
+const loadStore = () => {
+    throw new Error('No implementado');
+}
+
+/**
+ * 
+ * @param {string} todo  recibe y crea un nuevo todo
+ */
+
+const addTodo = (description) => {
+    throw new Error('No implementado');
+}
+
+/**
+ * 
+ * @param {string} todoId Recibe el id del todo para determinar si está completada o no. 
+ */
+
+const toggleTodo = (todoId) => {
+    throw new Error('No implementado');
+}
+
+/**
+ * 
+ * @param {string} todoId Recibe el id de la tarea que deseamos borrar y lo elimina
+ */
+
+const deletedTodo = (todoId) => {
+    throw new Error('No implementado');
+}
+
+const deletedCompleted = (todoId) => {
+    throw new Error('No implementado');
+}
+
+const setFilter = (newFilter = Filters.All) => {
+    throw new Error('No implementado');
+}
+
+const getCurrentFilter = () => {
+    throw new Error('No implementado');
+}
+
 export default {
     initStore,
+    loadStore,
+    addTodo,
+    toggleTodo,
+    deletedTodo,
+    deletedCompleted,
+    setFilter,
+    getCurrentFilter,   
 }
