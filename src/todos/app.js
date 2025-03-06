32061ff63673dc5d29f9ff8457335b01da9a7736
Daniel Naranjo <naranjobenavidesd@gmail.com>
2025-03-06 07:42:30 +0100

    se genera la función dispayTodos para mandar a llamar los todos y se genera el archivo de barril

--- src/todos/app.js
+++ src/todos/app.js
@@ -3,7 +3,14 @@
  * @param {string} elementId 
  */
 
+import todoStore from '../store/todo-store';
 import  html  from './app.html?raw';
+
+const displayTodos = () => {
+    const todos = todoStore.getTodos(todoStore.getCurrentFilter());
+    console.log(todos);
+    
+}
 
 export const App = (elementId) => {
 
@@ -12,6 +19,7 @@ export const App = (elementId) => {
         const app = document.createElement('div');
         app.innerHTML = html;
         document.querySelector(elementId).append(app);
+        displayTodos();
     })();
 
 }
