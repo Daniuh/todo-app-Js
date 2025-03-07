bdcc902b0a4bf4c85a6fc87b09a8619335c630e1
Daniel Naranjo <naranjobenavidesd@gmail.com>
2025-03-07 07:22:19 +0100

    Se arregla problema que no permitia pintar bien en el dom los todos y se arregla diseño

--- src/todos/use-cases/create-todo-html.js
+++ src/todos/use-cases/create-todo-html.js
@@ -5,12 +5,23 @@ import { Todo } from '../models/todo-model';
  * @param {Todo} todo 
  */
 export const createTodoHtml = (todo) => {
-    if(!todo) throw new Error('A TODO es requerido');
-    
-    const html = `<h1>${todo.description}</h1>`;
+    if (!todo) throw new Error('A TODO es requerido');
+
+    const html = `
+                <div class="view">
+                    <input class="toggle" type="checkbox" ${todo.done ? 'checked' : ''}>
+                    <label>Probar JavaScript</label>
+                    <button class="destroy"></button>
+                </div>
+                <input class="edit" value="Create a TodoMVC template">`;
 
     const liElement = document.createElement('li');
     liElement.innerHTML = html;
+    liElement.setAttribute('data-id', todo.id);
+
+    if(todo.done){
+    liElement.classList.add('completed');
+    }
 
     return liElement
 }
