44246161990ad416a486174f2fde3a5865ccafd3
Daniel Naranjo <naranjobenavidesd@gmail.com>
2025-03-07 07:36:09 +0100

    Se cambia el label para que se muestre por id la descripción

--- src/todos/use-cases/create-todo-html.js
+++ src/todos/use-cases/create-todo-html.js
@@ -7,19 +7,21 @@ import { Todo } from '../models/todo-model';
 export const createTodoHtml = (todo) => {
     if (!todo) throw new Error('A TODO es requerido');
 
+    const {done, description, id, createdAt} = todo; //Se desestructuriza para no estar escribiendo todo.done...
+
     const html = `
                 <div class="view">
-                    <input class="toggle" type="checkbox" ${todo.done ? 'checked' : ''}>
-                    <label>Probar JavaScript</label>
+                    <input class="toggle" type="checkbox" ${done ? 'checked' : ''}>
+                    <label>${description}</label>
                     <button class="destroy"></button>
                 </div>
                 <input class="edit" value="Create a TodoMVC template">`;
 
     const liElement = document.createElement('li');
     liElement.innerHTML = html;
-    liElement.setAttribute('data-id', todo.id);
+    liElement.setAttribute('data-id', id);
 
-    if(todo.done){
+    if(done){
     liElement.classList.add('completed');
     }
 
--- src/todos/use-cases/render-todos.js
+++ src/todos/use-cases/render-todos.js
@@ -12,6 +12,9 @@ export const renderTodos = (elementId, todos = []) => {
     if(!element){element = document.querySelector(elementId);}
 
     if(!element){throw new Error(`Element ${elementId} not found`)};
+
+    element.innerHTML = '';
+
     todos.forEach(todo => {
         element.append(createTodoHtml(todo));
     });
